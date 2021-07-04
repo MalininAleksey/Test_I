055e9e934bace193f3a0727388a835cf629f9ff1
Aleksey <malinin.aleksey95@mail.ru>
2021-07-04 14:00:32 +0700

    Add read function's

---
+++ main.py
@@ -0,0 +1,55 @@
+from csv import reader
+from json import load
+from xml.dom import minidom
+import xml.etree.ElementTree as ET
+
+
+class Reader:
+
+    def __init__(self, path):
+        self.path = path
+
+
+    def read_csv(self):
+        with open(self.path, encoding="utf-8") as r_file:
+            reader_object = reader(r_file, delimiter=",")
+            count = 0
+            final_list = []
+            for row in reader_object:
+                if count == 0:
+                    title = row
+                else:
+                    content = row
+                    tuple_list = [(title[i], v) for i, v in enumerate(content)]
+                    final_list.append(tuple_list)
+                count += 1
+            return final_list
+
+
+    def read_json(self):
+        with open(self.path, encoding="utf-8") as r_file:
+            reader_object = load(r_file)
+            final_list = []
+            for item in reader_object['fields']:
+                turple_list = [(key, value) for key, value in item.items()]
+                final_list.append(turple_list)
+            return final_list
+
+
+    def read_xml(self):
+        with open(self.path, encoding='utf-8') as r_file:
+            root = ET.parse(r_file).getroot()
+            final_list = []
+            for item in root:
+                tuple_list = [(sub_item.attrib['name'], sub_item[0].text) for sub_item in item]
+                final_list.append(tuple_list)
+            return final_list
+
+
+if __name__ == "__main__":
+    csv = Reader('csv_data_1.csv')
+    print(csv.read_csv())
+    json = Reader('json_data.json')
+    print(json.read_json())
+    xml = Reader('xml_data.xml')
+    print(xml.read_xml())
